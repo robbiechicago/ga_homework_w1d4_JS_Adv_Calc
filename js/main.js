757311bf7819633948a8d63c3aa298e579426a0b
Rob Holloway <robbiechicago@gmail.com>
2015-08-13 18:37:52 +0100

    grabbed operator value and established correct calculations of numbers for each operator choice

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-// console.log("hi");
-
 // function to work the basic calculator
-// function basicCalc () {
+  
+var basicCalcButton = document.getElementById('basic-calc');
+basicCalcButton.addEventListener ('click', function() {
   
   //variables getting the numbers, operator and calculate button
-  
-  var basicCalcButton = document.getElementById('basic-calc');
+  var goodNumOne = parseFloat(document.getElementById('basic-num-1').value);
+  var goodNumTwo = parseFloat(document.getElementById('basic-num-2').value);
+  var op = document.getElementById('basic-operation').value;
+  var result;
 
-  basicCalcButton.addEventListener ('click', function() {
-    var goodNumOne = parseFloat(document.getElementById('basic-num-1').value);
-    var goodNumTwo = parseFloat(document.getElementById('basic-num-2').value);
-    console.log('button clicked');
-    console.log(typeof(goodNumOne));
-    console.log(typeof(goodNumTwo));
-  });
+  //calculations
+  if (op === '+') {
+    result = goodNumOne + goodNumTwo;
+  } else if (op === '-') {
+    result = goodNumOne - goodNumTwo;
+  } else if (op === '*') {
+    result = goodNumOne * goodNumTwo;
+  } else {
+    result = goodNumOne / goodNumTwo;
+  }
 
-//}
+  console.log('button clicked');
+  console.log(goodNumOne);
+  console.log(op);
+  console.log(goodNumTwo);
+  console.log(result);
+
+
+});
+
