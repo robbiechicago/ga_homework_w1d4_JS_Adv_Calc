692a2003288deffd01691f6e964bc4c0124190ad
Rob Holloway <robbiechicago@gmail.com>
2015-08-13 19:14:34 +0100

    basic calc V1 complete - hidden div now shows when button is pressed and result is included therein.  V2 will, if necessary, check for invalid data submissions or blanks or empty strings or whatever the idiot user might throw at us

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 var basicCalcButton = document.getElementById('basic-calc');
 basicCalcButton.addEventListener ('click', function() {
   
-  //variables getting the numbers, operator and calculate button
+  //variables getting the numbers, operator and hidden div
   var goodNumOne = parseFloat(document.getElementById('basic-num-1').value);
   var goodNumTwo = parseFloat(document.getElementById('basic-num-2').value);
   var op = document.getElementById('basic-operation').value;
+  var resultDiv = document.getElementById('basic-answer');
   var result;
 
   //calculations
@@ -19,6 +20,10 @@ basicCalcButton.addEventListener ('click', function() {
   } else {
     result = goodNumOne / goodNumTwo;
   }
+
+  //actions
+  resultDiv.innerHTML = '<div id="basic-answer-alert" class="col-sm-offset-2 col-sm-2 alert alert-success">' + result + '</div>';
+  resultDiv.className = 'show';
 
   console.log('button clicked');
   console.log(goodNumOne);
